b83127b968a7894976af23cb5360ee0ebc9a7d88
akiva <hershkowitz116@gmail.com>
2016-01-19 12:46:20 -0500

    fixing the port

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+console.log(process.argv);
+if(process.argv.indexOf("--production") > -1){
+ port = 80;
+}
 
-app.listen(process.env.PORT , function () {
-  console.log('Example app listening on port' + process.env.PORT );
+app.listen(port , function () {
+  console.log('Example app listening on port ' + port );
 });
